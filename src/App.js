c9187c9c212d2be919df46c15bcc463c8b34ad99
Gleb Kilichenko <hlib.kilichenko@pixida.de>
2023-12-14 10:45:07 +0100

    initialization

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import Form, { withTheme } from '@rjsf/core';
+import { Theme as Bootstrap4Theme } from '@rjsf/bootstrap-4';
+import validator from '@rjsf/validator-ajv8';
+
+
+const FormWithTheme = withTheme(Bootstrap4Theme);
+const schema = {
+  title: "My Form",
+  type: "object",
+  required: ["firstName", "lastName"],
+  properties: {
+    firstName: {
+      type: "string",
+      title: "First name"
+    },
+    lastName: {
+      type: "string",
+      title: "Last name"
+    },
+    age: {
+      type: "integer",
+      title: "Age"
+    },
+    bio: {
+      type: "string",
+      title: "Bio"
+    },
+    password: {
+      type: "string",
+      title: "Password",
+      minLength: 3
+    }
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="form-container">
+        <FormWithTheme schema={schema} validator={validator} onSubmit={({ formData }) => console.log(formData)} />
+      </div>
     </div>
   );
 }
