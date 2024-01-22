8e5f7368fb26503e99317ff00ba6ac4c41756b60
Gleb Kilichenko <hlib.kilichenko@pixida.de>
2024-01-22 10:30:45 +0100

    requesting the schema from the backend

--- src/App.js
+++ src/App.js
@@ -1,23 +1,59 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import validator from '@rjsf/validator-ajv8';
 
-//import Form from '@rjsf/core'
-//import Form from '@rjsf/bootstrap-4'
-import Form from '@rjsf/antd'
+import Form from '@rjsf/core';
+//import Form from '@rjsf/bootstrap-4';
+//import Form from '@rjsf/antd';
 
 
 import MultiSelectWidget from './components/MultiSelectWidget';
-import schema from './dataset_schema.json'
-import UiSchema from './UIschema.json'
+import hardcoded_schema from './dataset_schema.json';
+import UiSchema from './UIschema.json';
 import regions from './data/regions.json';
-import formData from './data/formData.json'
+import formData from './data/formData.json';
+
+const fetchSchema = async () => {
+  try {
+    const response = await fetch('/api/v2/schema/?format=json');
+    const schemaDict = await response.json();
+
+    // Assume fetched schema is similar in structure to hardcoded schema
+    const fetchedProperties = schemaDict["components"]["schemas"]["Dataset"]["properties"];
+
+    // Filter fetched properties based on hardcoded schema
+    const cleanProperties = Object.keys(fetchedProperties)
+      .filter(key => key in hardcoded_schema.properties) // Check if property exists in hardcoded schema
+      .reduce((obj, key) => {
+        obj[key] = fetchedProperties[key];
+        return obj;
+      }, {});
+
+    // Prepare the clean schema to be returned
+    const clean_schema = {
+      title: "Metadata Editor",
+      type: "object",
+      properties: cleanProperties,
+    };
+
+    return clean_schema
+  } catch (error) {
+    console.error('Error fetching schema:', error);
+    return null;
+  }
+};
 
 
 function App() {
+  const [schema, setSchema] = useState(null); // State to hold the fetched schema
 
-  // const widgets = {
-  //   MultiSelect: (props) => <MultiSelectWidget {...props} options={regions} />
-  // };
+  useEffect(() => {
+    fetchSchema().then(fetchedSchema => {
+      if (fetchedSchema) {
+        setSchema(fetchedSchema); // Update the state with the fetched schema
+      }
+    });
+  }, []);
+
 
   const widgets = {
     MultiSelect: (props) => {
@@ -32,6 +68,13 @@ function App() {
       return <MultiSelectWidget {...props} options={options} />;
     }
   };
+
+  console.log(hardcoded_schema)
+  console.log(schema)
+
+  if (!schema) {
+    return <div>Loading schema...</div>; // Show loading state or a spinner
+  }
 
   return (
     <div className="App">
